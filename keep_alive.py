26ff9e4333e3b234d73248239ce332ef69201c6f
chimney-0 <145744083+chimney-0@users.noreply.github.com>
2023-11-13 12:56:33 +0500

    removed webhook handling

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,4 @@
-from flask import Flask, request, render_template
-import subprocess
+from flask import Flask, render_template
 from threading import Thread
 
 app = Flask(__name__)
@@ -7,14 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return "Alive"
-
-@app.route('/github-webhook', methods = ['POST'])
-def github_webhook():
-    if request.headers.get('X-GitHub-Event') == 'push':
-        subprocess.run(['git', 'pull'])
-        return 'Webhook received successfully', 200
-    else:
-        return 'Invalid webhook event', 400
 
 def run():
   app.run(host = '0.0.0.0', port = 8080)
