1b5a446f5b7cf628f94a75c5825d8784d8df0c0d
chimney-0 <145744083+chimney-0@users.noreply.github.com>
2023-11-13 12:15:23 +0500

    added auto pull script

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import subprocess
 from threading import Thread
 
 app = Flask(__name__)
@@ -7,9 +8,17 @@ app = Flask(__name__)
 def index():
   return "Alive"
 
+@app.route('/github-webhook', methods = ['POST'])
+def github_webhook():
+    if request.headers.get('X-GitHub-Event') == 'push':
+        subprocess.run(['git', 'pull'])
+        return 'Webhook received successfully', 200
+    else:
+        return 'Invalid webhook event', 400
+
 def run():
-  app.run(host='0.0.0.0', port=8080)
+  app.run(host = '0.0.0.0', port = 8080)
 
 def keep_alive():
-  t = Thread(target=run)
+  t = Thread(target = run)
   t.start()
